98becc4106b4f0e38aeac01bc044315857ea2406
exidna <dm.a.titov@ya.ru>
2025-03-27 00:52:36 +0300

    feat: set benches quality

--- crates/model/src/lib.rs
+++ crates/model/src/lib.rs
@@ -27,22 +27,34 @@ fn write_data_file(file: &std::fs::File, quality: u64) -> anyhow::Result<()> {
     Ok(())
 }
 
-pub fn list_data_files() -> anyhow::Result<impl Iterator<Item = std::path::PathBuf>> {
+pub fn list_data_files() -> anyhow::Result<impl Iterator<Item = (u64, std::path::PathBuf)>> {
     // IMPLEMENTATION NOTES:
     // We need to iterate over all `out_dir` entiries to catch any io errors.
     // Otherwise, we have to unwrap this values, which might be unexpected.
-    
+
     let out_dir = out_dir_path();
 
     let mut files = vec![];
     for entry in std::fs::read_dir(out_dir)? {
         let path = entry?.path();
         if path.is_file() {
-            files.push(path);
+            let quality = get_quality(&path)?;
+            files.push((quality, path));
         }
     }
 
     files.sort();
 
     Ok(files.into_iter())
+}
+
+fn get_quality(path: &std::path::Path) -> anyhow::Result<u64> {
+    let Some(stem) = path.file_stem() else {
+        anyhow::bail!("invalid data file path: {}", path.display());
+    };
+    let Some(stem) = stem.to_str() else {
+        anyhow::bail!("non ascii character in data file path");
+    };
+    let quality = stem[5..].parse::<u64>()?;
+    Ok(quality)
 }
