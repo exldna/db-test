e66c8abd4733592f170e3b4c042e639ef4f0718e
exidna <dm.a.titov@ya.ru>
2025-04-02 18:16:32 +0300

    fix[redis]: docker.sh

--- redis/docker.sh
+++ redis/docker.sh
@@ -7,7 +7,7 @@ docker container run -d --name $CONTAINER_NAME redis
 docker cp $(dirname $0)/run.sh $CONTAINER_NAME:/tmp
 cat $DATA_FILE | docker exec $CONTAINER_NAME tar -x -C /tmp
 
-docker exec /tmp/run.sh /tmp/items
+docker exec $CONTAINER_NAME /tmp/run.sh /tmp/items
 
 docker stop $CONTAINER_NAME
 docker rm $CONTAINER_NAME
