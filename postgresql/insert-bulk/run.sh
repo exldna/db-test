4cabce02890c8c8af0e6042cd69c1081f9e5cf63
exidna <dm.a.titov@ya.ru>
2025-04-02 20:21:59 +0300

    fix: change string table data type to text

--- postgresql/insert-bulk/run.sh
+++ postgresql/insert-bulk/run.sh
@@ -11,9 +11,9 @@ psql -U postgres -t -c "create database test"
 
 psql -U postgres -d test -t -c " \
     create table if not exists user_transactions ( \
-        user_addr character[26] not null, \
+        user_addr text not null, \
         tx_timestamp timestamp not null, \
-        tx_hash character[32] not null \
+        tx_hash text not null \
     );"
 
 time (
