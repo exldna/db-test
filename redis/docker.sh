a1451b9e27f0e656c8b6c0c4a0a9972077ba8f44
exidna <dm.a.titov@ya.ru>
2025-04-02 18:06:50 +0300

    feat[redis]: add docker.sh; results template

---
+++ redis/docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CONTAINER_NAME="bench-redis"
+DATA_FILE=$1
+
+docker container run -d --name $CONTAINER_NAME
+docker cp $(dirname $0)/run.sh $CONTAINER_NAME:/tmp
+cat $DATA_FILE | docker exec -i $CONTAINER_NAME tar -x -C /tmp
+
+docker exec -i /tmp/run.sh /tmp/items
+
+docker stop $CONTAINER_NAME
+docker rm $CONTAINER_NAME
