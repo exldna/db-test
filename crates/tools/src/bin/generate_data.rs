a1c3c0e6c163091d2e578dde485c51e50b983365
exidna <dm.a.titov@ya.ru>
2025-04-02 15:57:04 +0300

    fix[db-test-tools:generate_data]: create data folder

--- crates/tools/src/bin/generate_data.rs
+++ crates/tools/src/bin/generate_data.rs
@@ -4,6 +4,7 @@ fn main() -> anyhow::Result<()> {
     let config = Config::parse();
     let out_dir = &config.out_dir;
     let qualities = config.qualities.iter().copied();
+    std::fs::create_dir_all(out_dir)?;
     db_test_model::generate_data(out_dir, qualities)?;
     Ok(())
 }
