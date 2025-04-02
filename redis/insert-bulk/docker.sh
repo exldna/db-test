ba37793c94fde625474badd270c900532ef5e054
exidna <dm.a.titov@ya.ru>
2025-04-02 18:47:56 +0300

    feat[redis-insert-bulk]: produce bench results; reorganize bench struct

--- redis/insert-bulk/docker.sh
+++ redis/insert-bulk/docker.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+DATA_FILE="$1"
 CONTAINER_NAME="bench-redis"
-DATA_FILE=$1
 
 docker container run -d --name $CONTAINER_NAME redis
 docker cp $(dirname $0)/run.sh $CONTAINER_NAME:/tmp
-cat $DATA_FILE | docker exec $CONTAINER_NAME tar -x -C /tmp
+cat $DATA_FILE | docker exec -i $CONTAINER_NAME tar -x -C /tmp
 
-docker exec $CONTAINER_NAME /tmp/run.sh /tmp/items
+docker exec -i $CONTAINER_NAME /tmp/run.sh /tmp/items
 
 docker stop $CONTAINER_NAME
 docker rm $CONTAINER_NAME
