cd4014b2ba9764c0c3aa5c1481c5476fb334b99d
exidna <dm.a.titov@ya.ru>
2025-04-02 20:18:39 +0300

    fix: sudo command not exists

--- postgresql/insert-bulk/run.sh
+++ postgresql/insert-bulk/run.sh
@@ -7,16 +7,18 @@ until pg_isready -h localhost -p 5432; do
     sleep 1
 done
 
-sudo -u postgres psql -t -c "create database test"
+psql -U postgres -t -c "create database test"
 
-sudo -u postgres psql test -t -c " \
-create table if not exists user_transactions ( \
-    user_addr character[26] not null, \
-    tx_timestamp timestamp not null, \
-    tx_hash character[32] not null \
-);"
+psql -U postgres -d test -t -c " \
+    create table if not exists user_transactions ( \
+        user_addr character[26] not null, \
+        tx_timestamp timestamp not null, \
+        tx_hash character[32] not null \
+    );"
 
-sudo -u postgres time psql test -c " \
-COPY user_transactions(user_addr, tx_timestamp, tx_hash) \
-FROM '${DATA_FILE}' \
-WITH (FORMAT csv, HEADER true);"
+time (
+    psql -U postgres -d test -c " \
+        COPY user_transactions(user_addr, tx_timestamp, tx_hash) \
+        FROM '${DATA_FILE}' \
+        WITH (FORMAT csv, HEADER true);"
+)
