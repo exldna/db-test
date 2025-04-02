4e55cd4074b745d63dcff46ab14304925fa97b02
exidna <dm.a.titov@ya.ru>
2025-04-02 18:08:53 +0300

    fix[redis]: detached exec

--- redis/docker.sh
+++ redis/docker.sh
@@ -5,9 +5,9 @@ DATA_FILE=$1
 
 docker container run -d --name $CONTAINER_NAME
 docker cp $(dirname $0)/run.sh $CONTAINER_NAME:/tmp
-cat $DATA_FILE | docker exec -i $CONTAINER_NAME tar -x -C /tmp
+cat $DATA_FILE | docker exec $CONTAINER_NAME tar -x -C /tmp
 
-docker exec -i /tmp/run.sh /tmp/items
+docker exec /tmp/run.sh /tmp/items
 
 docker stop $CONTAINER_NAME
 docker rm $CONTAINER_NAME
