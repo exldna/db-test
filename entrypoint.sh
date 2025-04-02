e1005f15f3c826b404c06167a1c27b8f2bbce112
exidna <dm.a.titov@ya.ru>
2025-04-02 19:46:24 +0300

    fix[entrypoint]: fwd data file

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ TRIES=3
 
 SYSTEM="$1"
 BENCHMARK="$2"
+DATA_FILE="$3"
 
 # clear system caches
 sync
@@ -11,5 +12,5 @@ echo 3 | sudo tee /proc/sys/vm/drop_caches
 
 for i in $(seq 1 $TRIES)
 do
-    "./${SYSTEM}/${BENCHMARK}/docker.sh"
+    "./${SYSTEM}/${BENCHMARK}/docker.sh ${DATA_FILE}"
 done
