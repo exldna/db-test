01983a7eb55a73c34150ac04e71208e4f24205ba
exidna <dm.a.titov@ya.ru>
2025-04-02 20:13:03 +0300

    fix: postgres permissions

--- postgresql/insert-bulk/run.sh
+++ postgresql/insert-bulk/run.sh
@@ -2,21 +2,21 @@
 
 DATA_FILE="$1"
 
-until pg_isready -h localhost -p 5432 -U postgres; do
+until sudo -u postgres pg_isready -h localhost -p 5432; do
     echo "Waiting for PostgreSQL to start..."
     sleep 1
 done
 
-psql -t -c "create database test"
+sudo -u postgres psql -t -c "create database test"
 
-psql test -t -c " \
+sudo -u postgres psql test -t -c " \
 create table if not exists user_transactions ( \
     user_addr character[26] not null, \
     tx_timestamp timestamp not null, \
     tx_hash character[32] not null \
 );"
 
-time psql test -c " \
+sudo -u postgres time psql test -c " \
 COPY user_transactions(user_addr, tx_timestamp, tx_hash) \
 FROM '${DATA_FILE}' \
 WITH (FORMAT csv, HEADER true);"
