a4607886e4af139298b87d232dce254972e72c88
exidna <dm.a.titov@ya.ru>
2025-04-02 20:25:14 +0300

    fix: change timestamp data type to int

--- postgresql/insert-bulk/run.sh
+++ postgresql/insert-bulk/run.sh
@@ -12,7 +12,7 @@ psql -U postgres -t -c "create database test"
 psql -U postgres -d test -t -c " \
     create table if not exists user_transactions ( \
         user_addr text not null, \
-        tx_timestamp timestamp not null, \
+        tx_timestamp int not null, \
         tx_hash text not null \
     );"
 
@@ -20,5 +20,5 @@ time (
     psql -U postgres -d test -c " \
         COPY user_transactions(user_addr, tx_timestamp, tx_hash) \
         FROM '${DATA_FILE}' \
-        WITH (FORMAT csv, HEADER true);"
+        WITH (FORMAT csv);"
 )
