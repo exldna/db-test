d9cb2eb537a2a2f19220a220bc4843e4adeae2e6
exidna <dm.a.titov@ya.ru>
2025-04-04 15:54:29 +0300

    fix

--- postgresql/docker.sh
+++ postgresql/docker.sh
@@ -8,10 +8,12 @@ BENCH_DIR="$(dirname $0)/${BENCHMARK}"
 
 docker container run -d --name $CONTAINER_NAME -e POSTGRES_HOST_AUTH_METHOD=trust postgres
 
-docker cp $(dirname $0)/run.sh $CONTAINER_NAME:/tmp
 docker cp $DATA_FILE $CONTAINER_NAME:/tmp
 
-docker exec -i $CONTAINER_NAME /tmp/run.sh /tmp/$(basename $DATA_FILE)
+docker cp "${BENCH_DIR}/benchmark.sh" $CONTAINER_NAME:/tmp
+docker cp "${BENCH_DIR}/run.sh" $CONTAINER_NAME:/tmp
+
+docker exec -i $CONTAINER_NAME /tmp/benchmark.sh /tmp/$(basename $DATA_FILE)
 
 docker stop $CONTAINER_NAME
 docker rm $CONTAINER_NAME
